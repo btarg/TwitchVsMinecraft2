4a3ac4178d956ac8c251ff50edfbb8f30b776e86
iCrazyBlaze <iCrazyBlaze@users.noreply.github.com>
2021-02-19 13:05:52 +0000

    oops, add return statement for when mod is unloaded

--- src/main/java/io/github/icrazyblaze/twitchmod/integration/CarrierBeesIntegration.java
+++ src/main/java/io/github/icrazyblaze/twitchmod/integration/CarrierBeesIntegration.java
@@ -13,6 +13,10 @@ public class CarrierBeesIntegration {
     }
 
     public static void initDynamicCommands(String sender) {
+
+        if (!isCarrierBeesLoaded())
+            return;
+
         registerCommand(() -> CommandHandlers.spawnCarrierBee(sender, ModEntities.CARRIER_BEE.get()), "carrierbee", "bee");
         registerCommand(() -> CommandHandlers.spawnCarrierBee(sender, ModEntities.BOMBLE_BEE.get()), "bomblebee", "bee2");
         registerCommand(() -> CommandHandlers.spawnCarrierBee(sender, ModEntities.FUMBLE_BEE.get()), "fumblebee", "bee3");
